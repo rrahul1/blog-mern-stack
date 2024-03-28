206c1f5fe21c3ff1e084df4a5e71b557990ff8e3
Rahul Singh <rathore.rahul.1999@gmail.com>
2024-03-28 13:25:27 +0530

    signup api created

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
@@ -10,7 +12,11 @@ mongoose
    .catch((err) => console.log(err));
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
-   console.log("server is running ons port 3000!!");
+   console.log("server is running on port 3000!!");
 });
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
