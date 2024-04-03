08235293f97611b7f823a2a8ceccfd603f804ea9
Rahul Singh <rathore.rahul.1999@gmail.com>
2024-04-03 16:37:31 +0530

    complete getuser functionality

--- frontend/src/components/DashSidebar.jsx
+++ frontend/src/components/DashSidebar.jsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Sidebar } from "flowbite-react";
-import { HiArrowSmRight, HiDocumentText, HiUser } from "react-icons/hi";
+import {
+   HiArrowSmRight,
+   HiDocumentText,
+   HiOutlineUserGroup,
+   HiUser,
+} from "react-icons/hi";
 import { Link, useLocation } from "react-router-dom";
 import { signOutSuccess } from "../redux/user/userSlice";
 import { useDispatch, useSelector } from "react-redux";
@@ -63,6 +68,17 @@ const DashSidebar = () => {
                            as="div"
                         >
                            Posts
+                        </Sidebar.Item>
+                     </Link>
+                  )}
+                  {currentUser.isAdmin && (
+                     <Link to="/dashboard?tab=users">
+                        <Sidebar.Item
+                           active={tab === "users"}
+                           icon={HiOutlineUserGroup}
+                           as="div"
+                        >
+                           Users
                         </Sidebar.Item>
                      </Link>
                   )}
