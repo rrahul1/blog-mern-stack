d15ae9c4c911778d04fc8f70b79d66fd7dc61f12
Rahul Singh <rathore.rahul.1999@gmail.com>
2024-04-04 23:39:39 +0530

    added projects and about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,47 @@
 import React from "react";
 
 const About = () => {
-   return <div>About</div>;
+   return (
+      <div className="min-h-screen flex items-center justify-center">
+         <div className="max-w-2xl mx-auto p-3 text-center">
+            <h1 className="text-3xl font font-semibold text-center my-7">
+               About Wish's Blog
+            </h1>
+            <div className="text-md text-gray-500 flex flex-col gap-6">
+               <p>
+                  Welcome to Wish's Blog, where inspiration meets expression!
+                  Dive into a world of captivating articles, insightful
+                  narratives, and thought-provoking discussions. Powered by the
+                  latest MERN (MongoDB, Express.js, React.js, Node.js)
+                  technology stack, our platform offers a seamless and dynamic
+                  user experience.
+               </p>
+               <p>
+                  Explore a diverse range of topics curated by passionate
+                  writers from around the globe. Whether you're seeking expert
+                  advice on technology trends, insightful analysis of current
+                  events, or heartfelt stories that resonate with your soul,
+                  you'll find it all here. With intuitive navigation and a
+                  sleek, modern interface, discovering new content has never
+                  been easier.
+               </p>
+               <p>
+                  Join our vibrant community of readers and writers, where ideas
+                  flow freely and creativity knows no bounds. Engage with fellow
+                  enthusiasts through comments, likes, and shares, fostering
+                  meaningful connections and expanding your horizons.
+               </p>
+               <p>
+                  Whether you're a seasoned blogger, an avid reader, or someone
+                  simply seeking inspiration, Wish's Blog invites you to embark
+                  on a journey of discovery, enlightenment, and connection.
+                  Start exploring today and unleash the power of your
+                  imagination!
+               </p>
+            </div>
+         </div>
+      </div>
+   );
 };
 
 export default About;
