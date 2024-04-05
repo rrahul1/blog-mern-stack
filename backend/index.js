ad040dd3d0b007a6e103db68ff8e104bedf850d1
Rahul Singh <rathore.rahul.1999@gmail.com>
2024-04-05 11:44:53 +0530

    fix

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ app.use("/api/comment", commentRoute);
 
 app.use(express.static(path.join(__dirname, "/client/dist")));
 app.get("*", (req, res) => {
-   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 app.use((err, req, res, next) => {
